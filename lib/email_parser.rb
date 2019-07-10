2ed9a03c1392fda3a3c32582f7ee6bd8a2653d39
Amanda King <amapickell@gmail.com>
2019-07-10 20:58:32 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,11 +7,11 @@ class EmailParser
   attr_accessor :emails
   
   def initialize (emails)
-    @emails = emails.split(/, | /)
+    @emails = emails
   end
   
   def parse
-    @emails.uniq
+    @emails.split(/, | /).uniq
   end
   
 end
